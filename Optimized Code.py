f3b0640c486533c94c910546debada1699d8b0e8
hansdiamante <hans.diamante.cics@ust.edu.ph>
2023-02-22 11:21:52 +0800

    Made Resin backbutton work

--- Optimized Code.py
+++ Optimized Code.py
@@ -207,11 +207,46 @@ text_box2.grid(row=3, column=1, padx=10, pady=10)
 op_box.grid(row=0, rowspan=4, column=2, sticky="w", padx=10, pady=10)
 
 #backwards
+def reswantcalc():
+    nowdatetime = datetime.datetime.now()
+    reshav = resin_have.get()
+    date = date_picker.get_date()
+    resin_month = date.month
+    resin_day = date.day
+    resin_year = date.year
+    resin_hour = float(time_picker.hours())
+    nowhr = nowdatetime.hour
+    resin_minute = float(time_picker.minutes())
+
+    op_box_val = text_box2.get("1.0", tk.END)
+    op_box_val = op_box_val.replace('\n','')
+
+    wantdatetime = nowdatetime + datetime.timedelta(minutes=(int(op_box_val)-int(reshav))*8)
+    wantdatetime_str = wantdatetime.strftime(personal_dtformat)
+    nowdatetime_str = nowdatetime.strftime(personal_dtformat)
+
+    #converts hours from 12h to 24h
+    if(time_picker.period()=="a.m" and time_picker.hours()==12):resin_hour = 0
+    elif(time_picker.period()=="p.m" and time_picker.hours()<12):resin_hour += 12
+    nowmin = nowdatetime.minute
+    
+
+    op_box.delete("1.0", tk.END)
+    op_box.insert(tk.END, ""
+        +f"{reshav}->{op_box_val} Resin\n"
+        +"from\n"
+        +f"{nowdatetime_str}\n"
+        +"to\n"
+        +f"{wantdatetime_str}\n"
+        +f"Condensed Resin: {math.floor(float(op_box_val)/40)}\nFragile Resin: {math.floor(float(op_box_val)%40/20)}"
+        )
+
+
+
 resin_back_button = tk.Button(tab2, text="Resin Calculate",
-                            #   command=printshit,
+                              command=reswantcalc,
                                width=15, height=1)
 resin_back_button.grid(row=4, column=1, padx=10)
-
 time_picker.grid(row=4, column=0,sticky="w",padx=10, pady=10)
 
 
@@ -236,7 +271,7 @@ def printshit():
     text_box1.delete("1.0", tk.END)
     text_box1.insert(tk.END, fullrestime.strftime(personal_dtformat))
     
-    #calculate amount resin, from resin amount now, on a certain datetime
+    #get date and time values
     date = date_picker.get_date()
     resin_month = date.month
     resin_day = date.day
